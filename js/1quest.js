40a8a878b6a29b534634ecd560576308d3e8fb4f
Linokan <Andrei125.ivanov@mail.ru>
2024-09-28 22:37:09 +0700

    2 код добавлен

--- js/1quest.js
+++ js/1quest.js
@@ -2,5 +2,5 @@ let a = prompt("Введите число a:");
 let b = prompt("Введите число b:");
 a = parseFloat(a);
 b = parseFloat(b);
-arithmeticMean = (a+b)/2;
-alert(arithmeticMean);
+let arithmeticMean = (a+b)/2;
+alert("Среднее арифметическое чисел " + a + " и " + b + " = " + arithmeticMean);
---
+++ js/2quest.js
@@ -0,0 +1,4 @@
+let twoDigit = prompt("Введите двузначное число: ");
+twoDigit = parseFloat(twoDigit);
+let reverseTwoDigit = string(Math.floor(twoDigit / 10)) + string(twoDigit % 10);
+alert(reverseTwoDigit)
